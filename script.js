06fcf8349722d7aff4262a96e159bacf25d49271
Jacob Guiro <jguiro09@gmail.com>
2021-06-28 18:05:23 -0500

    First commit/Rows are done inside the index

---
+++ script.js
@@ -0,0 +1,31 @@
+// GIVEN I am using a daily planner to create a schedule
+
+
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+//     moments.js 
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+//     grid system: rows and col
+//                  12 col, # of rows
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+//      if (currenthour == true)
+//      .addclass (1-3)
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+//      <input> / <text area>
+//      event.target
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+//      DOM Manipulation
+//      Give each input an ID that saves the info
+
+// WHEN I refresh the page
+// THEN the saved events persist
+//      Localstorage get, if no local storage leave alone.
