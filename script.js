5d168e6107924f381c2d2aaac29f9c7905ee9a76
Jacob Guiro <jguiro09@gmail.com>
2021-06-28 19:01:05 -0500

    moment JS has been implemented, rows assigned with variables

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
 // THEN I am presented with timeblocks for standard business hours
 //     grid system: rows and col
 //                  12 col, # of rows
+// DONE-----------------------------------------------------------------------------------------------------------------
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
@@ -29,3 +30,8 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 //      Localstorage get, if no local storage leave alone.
+
+var topHTML = $('#currentDay');
+topHTML.text(moment().format("MMM Do, YYYY"));
+
+console.log(topHTML.text(moment().format("MMM Do, YYYY")));
