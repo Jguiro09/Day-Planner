636656f69178fb74bbe25486da894bd767584a6e
Jacob Guiro <jguiro09@gmail.com>
2021-06-29 19:38:59 -0500

    fixed time, had to switch to military time :(

--- script.js
+++ script.js
@@ -17,13 +17,14 @@
 // THEN I can enter an event
 //      <input> / <text area>   
 //      event.target
-// DONE-----------------------------------------------------------------------------------------------------------------
+
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 //      if (currenthour == true)
 //      .addclass (1-3)
 
+// DONE-----------------------------------------------------------------------------------------------------------------
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
 //      DOM Manipulation
@@ -33,28 +34,32 @@
 // THEN the saved events persist
 //      Localstorage get, if no local storage leave alone.
 
+checkTime();
+
+// GLOBAL VARIABLES
 var topHTML = $('#currentDay');
 var hourHTML = $('.hour');
 var rowHTML = $('.row');
 var description = $('.description');
 var todo = ["","","","","","","","",""];
 
-topHTML.text(moment().format("MMM Do, YYYY"));
-console.log(($('#0')).children(1).html());
-checkTime();
-
+//Assigns Time
+topHTML = topHTML.text(moment().format("MMMM Do YYYY"));
 
 // Checks the tiem and assigns the color to which it represents
 // Time past = Gray, Current Time = red, Time future = green
 // REASON FOR IDS: The IDS are used to run through each ID, the problem with the classes we have right now that is if we assign to it itll only go once since they all share the same name
+console.log(moment().format('H'));
+
 function checkTime()
 {
     $('.time-block').each(function ()
     {
-        if (parseInt(($(this)).attr('id')) == moment().format('h'))
+        console.log(parseInt(($(this)).attr('id')));
+        if (parseInt(($(this)).attr('id')) == moment().format('H'))
         {$(this).children('.description').addClass('present');}
 
-        else if (parseInt(($(this)).attr('id')) > moment().format('h'))
+        else if (parseInt(($(this)).attr('id')) > moment().format('H'))
         {$(this).children('.description').addClass('future');}
 
         else
@@ -65,10 +70,11 @@ function checkTime()
 function checkTodo()
 {
     check = localStorage.getItem("saved");
-    for(var i = 0; i < description.length; i++)
+    
+    $('.time-block').each(function ()
     {
-
-    }
+        
+    })
 }
 
 $('.saveBtn').click(function (event) 
